1b0920515cd10d180cc4cb1c8897ad5e24314d99
humulla <humairaa.mulla@hackney.gov.uk>
2020-06-18 15:40:55 +0100

    postgres setup for staging added

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -48,6 +48,27 @@ data "aws_ssm_parameter" "uh_postgres_db_password" {
    name = "/uh-api/staging/postgres-username"
  }
 
+ module "postgres_db_staging" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/postgres"
+  environment_name = "staging"
+  vpc_id = data.aws_vpc.staging_vpc.id
+  db_identifier = "uh-mirror"
+  db_name = "uh_mirror"
+  db_port  = 5302
+  subnet_ids = data.aws_subnet_ids.staging.ids
+  db_engine = "postgres"
+  db_engine_version = "11.1" //DMS does not work well with v12
+  db_instance_class = "db.t2.micro"
+  db_allocated_storage = 20
+  maintenance_window ="sun:10:00-sun:10:30"
+  db_username = data.aws_ssm_parameter.uh_postgres_username.value
+  db_password = data.aws_ssm_parameter.uh_postgres_db_password.value
+  storage_encrypted = false
+  multi_az = false //only true if production deployment
+  publicly_accessible = false
+  project_name = "platform apis"
+}
+
 /*    DMS SET UP    */
 
 data "aws_ssm_parameter" "uh_test_username" {
