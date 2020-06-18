30ce21ad05022e0970aa1422d088ab9fb5079021
humulla <humairaa.mulla@hackney.gov.uk>
2020-06-18 13:04:39 +0100

    Removed the AWS roles from staging/main.tf

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -76,23 +76,6 @@ data "aws_ssm_parameter" "uh_test_hostname" {
    }
  }
 
- resource "aws_iam_role" "dms-access-for-endpoint" {
-   assume_role_policy = "${data.aws_iam_policy_document.dms_assume_role.json}"
-   name               = "dms-access-for-endpoint"
- }
- resource "aws_iam_role_policy_attachment" "dms-access-for-endpoint-AmazonDMSRedshiftS3Role" {
-   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSRedshiftS3Role"
-   role       = "${aws_iam_role.dms-access-for-endpoint.name}"
- }
- resource "aws_iam_role" "dms-vpc-role" {
-   assume_role_policy = "${data.aws_iam_policy_document.dms_assume_role.json}"
-   name               = "dms-vpc-role"
- }
- resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRole" {
-   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
-   role       = "${aws_iam_role.dms-vpc-role.name}"
- }
-
 /* DMS SET UP INCLUDING DMS INSTANCE AS NONE EXISTS */
 
 module "dms_setup_staging" {
