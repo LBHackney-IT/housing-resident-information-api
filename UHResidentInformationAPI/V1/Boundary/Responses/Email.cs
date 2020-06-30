9ef4da84a3b237ec12f37d47bfc47313074110ba
humulla <humairaa.mulla@hackney.gov.uk>
2020-06-30 15:19:07 +0100

    Email response file added

---
+++ UHResidentInformationAPI/V1/Boundary/Responses/Email.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace UHResidentInformationAPI.V1.Boundary.Responses
+{
+    public class Email
+    {
+        public string EmailAddress { get; set; }
+        public string LastModified { get; set; }
+    }
+}
