8317b107c2a9821ddf2d0cdf9b23402fc94b744f
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-06-30 09:13:04 +0100

    amend list query

--- UHResidentInformationAPI/V1/Gateways/UHGateway.cs
+++ UHResidentInformationAPI/V1/Gateways/UHGateway.cs
@@ -21,16 +21,19 @@ namespace UHResidentInformationAPI.V1.Gateways
 
         public List<ResidentInformation> GetAllResidents(string houseReference = null, string firstName = null, string lastName = null, string address = null)
         {
+            //Inner join on person and address
             var listOfPerson = _uHContext.Persons
                 .Where(a => string.IsNullOrEmpty(houseReference) || a.HouseRef.Contains(houseReference))
                 .Where(a => string.IsNullOrEmpty(firstName) || a.FirstName.ToLower().Contains(firstName.ToLower()))
-                .Where(a => string.IsNullOrEmpty(lastName) || a.LastName.Contains(lastName))
+                .Where(a => string.IsNullOrEmpty(lastName) || a.LastName.ToLower().Contains(lastName.ToLower()))
                 .Join(
                 _uHContext.Addresses,
                 person => person.HouseRef,
                 address => address.HouseRef,
-                (person, address) => new { person, address })
-                .ToList()
+                (person, address) => new { person, address });
+
+            //Left join on listOfPerson and PhoneNumbers
+            var listOfResident = listOfPerson.ToList()
                 .GroupJoin
                 (
                     _uHContext.TelephoneNumbers,
@@ -38,21 +41,14 @@ namespace UHResidentInformationAPI.V1.Gateways
                     telephone => telephone.ContactID,
                     (anon, telephone) =>
                     {
-                        var p = anon.person.ToDomain();
-                        p.ResidentAddress = anon.address.ToDomain();
-                        p.PhoneNumber = telephone.Any() ? telephone.ToList().ToDomain() : null;
-                        return p;
+                        var resident = anon.person.ToDomain();
+                        resident.ResidentAddress = anon.address.ToDomain();
+                        resident.PhoneNumber = telephone.Any() ? telephone.ToList().ToDomain() : null;
+                        return resident;
                     }
-                )
-                .ToList();
+                ).ToList();
 
-            return listOfPerson;
+            return listOfResident;
         }
-
-        private static string StripString(string str)
-        {
-            return str?.ToLower().Replace(" ", "");
-        }
-
     }
 }
