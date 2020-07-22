ddbe6b0556221f34fb4a0dc50e811471f616bd52
Renny Fadoju <rennyvbirds@yahoo.com>
2020-07-22 10:32:50 +0100

    Change column reference in infrastructure to match table name case

--- UHResidentInformationAPI/V1/Infrastructure/EmailAddresses.cs
+++ UHResidentInformationAPI/V1/Infrastructure/EmailAddresses.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UHResidentInformationAPI.V1.Infrastructure
 {
-    [Table("ccemailaddress", Schema = "dbo")]
+    [Table("CCEmailAddress", Schema = "dbo")]
     public class EmailAddresses
     {
         [Column("contactno")]
