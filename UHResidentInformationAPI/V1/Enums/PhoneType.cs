8f14a8940b444ff029d2ca513b0d0bd6ec01008c
Renny Fadoju <rennyvbirds@yahoo.com>
2020-07-23 17:27:55 +0100

    Revert "Revert "Change Phone Type enums to match values in DB""
    This reverts commit f4ff47f6b7acfc3a82e4b0086c1f09d9dcaf6fe9.

--- UHResidentInformationAPI/V1/Enums/PhoneType.cs
+++ UHResidentInformationAPI/V1/Enums/PhoneType.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
+
 namespace UHResidentInformationAPI.V1.Enums
 {
     public enum PhoneType
     {
-        Primary,
-        Home,
-        Mobile,
-        Fax
+        //Correct descriptions??
+
+        [Description("Home")] H,
+        [Description("Mobile")] M,
+        [Description("Fax")] F,
+        [Description("Work")] W,
+        X,
     }
 }
