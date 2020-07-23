373429a4ea41b105b777c58ad60ddbfe91babdf6
Renny Fadoju <rennyvbirds@yahoo.com>
2020-07-23 14:02:19 +0100

    Change Phone Type enums to match values in DB

--- UHResidentInformationAPI/V1/Enums/PhoneType.cs
+++ UHResidentInformationAPI/V1/Enums/PhoneType.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel;
+
 namespace UHResidentInformationAPI.V1.Enums
 {
     public enum PhoneType
     {
-        Primary,
-        Home,
-        Mobile,
-        Fax
+        [Description("Home")] H,
+        [Description("Mobile")] M,
+        [Description("Fax")] F,
+        [Description("Work")] W,
+        X
     }
 }
